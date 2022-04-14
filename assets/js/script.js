49fc3a3b34c1af1f9b77900005ad34af45101b71
DreadHeadDev <45825566+odisclemons@users.noreply.github.com>
2022-04-14 11:18:29 -0400

    Revert "Changed some HTML in JS for yelp cards"

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,12 +28,12 @@ var CatList = document.querySelector('#cat-list');
 var CitySearchFun = function (event) {
 
   event.preventDefault();
-  if (!SearchBox.value || SearchBox.value.length < 2) {
+  if(!SearchBox.value || SearchBox.value.length < 2) {
     alert('You must specify a city.')
     return;
   }
 
-  if (!CatList.value) {
+  if(!CatList.value){
     alert('You must specify a category.')
     return;
   }
@@ -43,9 +43,9 @@ var CitySearchFun = function (event) {
 
 
 
+  
 
-
-}
+} 
 
 CitySubmit.addEventListener('click', CitySearchFun)
 
@@ -59,17 +59,17 @@ function createPlacesList() {
 
   var jQueryPlaces = document.querySelector('#sortable');
 
-  for (var i = 0; i < jQueryPlaces.children.length; i++) {
+  for (var i = 0; i <jQueryPlaces.children.length; i++) {
 
     var listLongitude = jQueryPlaces.children[i].getAttribute('data-longitude')
     var listLatitude = jQueryPlaces.children[i].getAttribute('data-latitude')
 
     places.push({ lat: listLatitude, lng: listLongitude })
 
-    console.log(places)
+   console.log(places)
 
 
-
+   
 
 
 
@@ -89,11 +89,11 @@ JquerySubmit.addEventListener('click', createPlacesList)
 
 /* ------------------------ click btn to add to list ------------------------ */
 
-function handleAddClick(i) {
+function handleAddClick(i){
   console.log(i)
   console.log(businesses[i])
-  let { latitude, longitude } = businesses[i].coordinates
-  let { name, image_url } = businesses[i]
+  let {latitude, longitude} = businesses[i].coordinates
+  let {name, image_url} = businesses[i]
   // adding to j query UI 
 
   var newJquryuiItem = `
@@ -104,12 +104,12 @@ function handleAddClick(i) {
 
      
       `
-  $('#sortable').append(newJquryuiItem)
+      $('#sortable').append(newJquryuiItem)
 
 
 
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: latitude, lng: longitude },
+    center: {lat: latitude, lng: longitude},
     zoom: 17,
   });
   places.push(businesses[i])
@@ -119,18 +119,18 @@ function handleAddClick(i) {
 
 function fetchApiData(term1, city) {
 
-  var bearer_token =
+var bearer_token =
     "KNzo5Qc9AI4wBhsJRiQb47rkb3LmpBO6LCIrWXFTEXnO9gAH0hUhvx7Em0iYsE1AQL3_FfHiq__AJaQawUsPl8TNjN747zm7XczRFIDVYIaUAmATp_LD8gdKvmdUYnYx";
 
-  // var city = 'toronto'
-  // var term1 = 'shopping'
+    // var city = 'toronto'
+    // var term1 = 'shopping'
 
-  console.log(city + "in fetchApiData")
+    console.log(city + "in fetchApiData")
 
 
   var url =
-    'https://floating-headland-95050.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=' + city + '&term=' + term1;
-  // console.log(location1)
+    'https://floating-headland-95050.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=' + city + '&term=' + term1 ;
+    // console.log(location1)
   var bearer = "Bearer " + bearer_token;
   fetch(url, {
     method: "GET",
@@ -142,27 +142,25 @@ function fetchApiData(term1, city) {
       return response.json();
     })
     .then(function (data) {
-
+      
       console.log(data);
       console.log('is it working')
 
       console.log(data.businesses[0].alias)
       businesses = data.businesses;
       $('#bus').html(null)
+      
+    
 
 
+    // render data
+    for (var i = 0; i < 20; i++) {
 
-
-
-      // render data
-      for (var i = 0; i < 20; i++) {
-
-        let { name, url, display_phone, image_url } = data.businesses[i]
-        if (!image_url) image_url = "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"
+      let {name, url, display_phone, image_url} = data.businesses[i]
+      if(!image_url) image_url = "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"
 
       /* ----------------------------------Yelp Cards---------------------------------------- */
       var newBox = `
-
 <div class="card">
  
   <div class="card-content">
@@ -172,42 +170,46 @@ function fetchApiData(term1, city) {
           <img src="${image_url}" alt="Placeholder image">
         </figure>
       </div>
+      
+  
+      </div>
+    
+      <button class="modal-close is-large" aria-label="close"></button>
+    </div>
+
+</div>
+
 
       <div class="media-content">
-        <h1 class="title is-4">${name}</h1>
+        <p class="title is-4">${name}</p>
         <p class="subtitle is-6">${display_phone}</p>
-        <button class="button is-rounded is-small js-modal-trigger" data-target="modal-js-example">
+        <button class="js-modal-trigger" data-target="modal-js-example">
         Open JS example modal
-        </button>
-        <div class="content">
-          <button name="btn-${i}" class="addToList button is-rounded is-small" onclick="handleAddClick(${i})">Add to list</button>
-          <br>
-          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-        </div>
+      </button>
       </div>
+    </div>
+
+    <div class="content">
+    <button name="btn-${i}" class="addToList" onclick="handleAddClick(${i})">Add to list</button>
+      <a href="#">#css</a> <a href="#">#responsive</a>
+      <br>
+      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
     </div>
   </div>
 </div> `
 
-
-        $('#bus').append(newBox)
-      }
+     
+      $('#bus').append(newBox)
+    }
     });
 
-  // .catch((error) => console.log(error));
+    // .catch((error) => console.log(error));
 
 
 
 
 }
 
-
-/* -------------------------- code to come back to -------------------------- */
-/* <div class="card-image">
-<figure class="image is-4by3">
-  <img src="${image_url}" alt="Placeholder image">
-</figure>
-</div> */
 
 
 
@@ -217,15 +219,15 @@ function fetchApiData(term1, city) {
 
 
 var formCatList = function (event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  var category = CatList.value.trim();
+    var category = CatList.value.trim();
 
-  console.log(category)
+    console.log(category)
 
 
 
-  fetchApiData(category, city)
+    fetchApiData(category, city)
 }
 
 
@@ -235,7 +237,13 @@ CatList.addEventListener('change', formCatList);
 
 /* ------------------------------ Sortable list ----------------------------- */
 
-$(function () {
-  $("#sortable").sortable();
-});
+$( function() {
+  $( "#sortable" ).sortable();
+} );
+
+
+
+
+
+
 
